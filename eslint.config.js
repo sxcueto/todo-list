4763306ccfeb27e3a2491510b0c47eaf770378ec
sxcueto <sxcueto@gmail.com>
2025-07-15 19:44:36 -0400

    added list to app and readme info

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    settings: { react: { version: 'detect' } },
+    extends: [
+      js.configs.recommended,
+      react,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+       
+      },
+    },
+    rules: {
+        'no-unused-vars': 'warn', //this changes the error to a warning
+        'react/prop-types': 'off', //this suppresses warnings about not using prop-types
+    },
+  },
+])
