6dc8846cccefc096046509537c108b30a29780c0
sxcueto <sxcueto@gmail.com>
2025-08-04 23:24:27 -0400

    final revisions

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import { useState } from "react";
 
 function App() {
   const [newTodo, setNewTodo] = useState('New todo will be here');
+  
+    const todos = [
+    {id: 1, title: "clean room"},
+    {id: 2, title: "take out trash"},
+    {id: 3, title: "do laundry"},
+  ] 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
       <p>{newTodo}</p>
-      <TodoList  />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,10 +1,10 @@
 function TodoListItem({ todo }) {
   return (
-    <div>
+    
     <li>
     {todo.title}
     </li>
-    </div>
+  
   );
 }
 
