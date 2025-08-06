6b0e3476c838327a091b8dc4c7d648b9666b7841
sxcueto <sxcueto@gmail.com>
2025-08-05 23:06:27 -0400

    first push adding basic hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,30 @@ import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import { useState } from "react";
 
+
 function App() {
-  const [newTodo, setNewTodo] = useState('New todo will be here');
-  
-    const todos = [
+  const [todoList, setTodoList] = useState([]);
+   const todos = [
     {id: 1, title: "clean room"},
     {id: 2, title: "take out trash"},
     {id: 3, title: "do laundry"},
-  ] 
+  ];
+  
+  function handleAddTodo(title){
+
+  const newTodo = { title, id: Date.now()};// uses title and id as keys
+  
+setTodoList([...todoList,newTodo]);
+
+ }  
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
-      <TodoList todos={todos} />
+       <TodoForm onAddTodo={handleAddTodo} /> 
+      <TodoList todos={todoList} />
     </div>
   );
-}
+ 
 
+}
 export default App;
