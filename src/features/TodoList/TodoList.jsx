b5d11765da235cc16ccea7df41ba1479175b35a4
sxcueto <sxcueto@gmail.com>
2025-08-26 21:08:44 -0400

    fixed onUpdateTodo

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -2,14 +2,14 @@ import TodoListItem from "../TodoList/TodoListItem";
 
 
 
-function TodoList({ todoList, onCompleteTodo }) {
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
   const filteredTodoList = todoList.filter(todo => !todo.isCompleted);
 
   return (
    filteredTodoList.length === 0 ? <p>Add todo above to get started.</p> :
     <ul>
       {filteredTodoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} onUpdateTodo={updateTodo}/>
+        <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} onUpdateTodo={onUpdateTodo}/>
       ))}
     </ul>
   );
